47165ed6cb311abe3c43b9740bca2e03e6a449af
erebelo <eduldrebelo@gmail.com>
2024-03-25 18:01:59 -0300

    Updating health check endpoint path

--- src/main/java/com/erebelo/springh2demo/constants/BusinessConstants.java
+++ src/main/java/com/erebelo/springh2demo/constants/BusinessConstants.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class BusinessConstants {
 
-    public static final String HEALTH_CHECK = "/healthcheck";
+    public static final String HEALTH_CHECK = "/health-check";
     public static final String CUSTOMER = "/customer";
     public static final String PRODUCT = "/product";
     public static final String ORDER = "/order";
