ed60461b399352afb60aa4ce53076525ecf74d04
erebelo <eduldrebelo@gmail.com>
2024-03-27 23:14:03 -0300

    Enabling transaction management rollback

--- src/main/java/com/erebelo/springh2demo/SpringH2DemoApplication.java
+++ src/main/java/com/erebelo/springh2demo/SpringH2DemoApplication.java
@@ -2,14 +2,13 @@ package com.erebelo.springh2demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class SpringH2DemoApplication {
 
 	public static void main(String[] args) {
-
-		System.out.println("Initializing...");
 		SpringApplication.run(SpringH2DemoApplication.class, args);
 	}
-
 }
